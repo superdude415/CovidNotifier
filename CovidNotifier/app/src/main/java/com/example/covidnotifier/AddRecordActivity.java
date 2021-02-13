f7c88da3cf8524a4c242ad16e0df063283c8520d
Nicholas Skoczylas <skoczylasn415@gmail.com>
2021-02-13 00:02:29 -0500

    First Commit: Basic App UI

---
+++ CovidNotifier/app/src/main/java/com/example/covidnotifier/AddRecordActivity.java
@@ -0,0 +1,13 @@
+package com.example.covidnotifier;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddRecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_record);
+    }
+}
